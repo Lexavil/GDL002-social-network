2cfb7abf6fc7fcfd230411a23fda31bc065b3e8f
ClaudiaGarfias <me@claudiagarfias.works>
2019-04-03 08:59:00 -0600

    PEER PROGRAMMING: Eliminamos carpetas inncesarias

--- GDL002-social-network-Funcionalidad-firebase/src/config/config.js
+++
@@ -1,9 +0,0 @@
- // Initialize Firebase
- let config = {
-    apiKey: "AIzaSyAG-k8sfR7HI-GHNFHv5mQlfb4Bu8zvJIY",
-    authDomain: "social-network-a702c.firebaseapp.com",
-    databaseURL: "https://social-network-a702c.firebaseio.com",
-    projectId: "social-network-a702c",
-    storageBucket: "social-network-a702c.appspot.com",
-    messagingSenderId: "99341463255"
-  };
--- GDL002-social-network-Funcionalidad-firebase/src/view.js
+++
@@ -1,9 +0,0 @@
-function router() {
-    if(firebase.auth().currentUser) {
-        location.hash = "#timeline"
-    }
-    else{
-        location.hash = "#login";
-    }
-}
-
